ad9a06269b1b3f0128deafa365410dbcd9a3033b
TomiHenriksson8 <tomihen01@gmail.com>
2023-11-13 14:07:43 +0200

    login user logic added

--- front-end/src/scripts/user/user.ts
+++ front-end/src/scripts/user/user.ts
@@ -9,15 +9,14 @@ const fetchGetExitingUsers = async () => {
     // TODO: add error handling later
 };
 
+
+// REGISTER A NEW USER
+
 const fetchPostNewUser = async (newUser: User) => {
     const existingUsers = await fetchGetExitingUsers();
-    
     const newUserId = getLatestUserId(existingUsers) + 1;
-
-    // Set the new ID and role in the newUser object
     newUser.id = newUserId;
     newUser.role = 'user';
-
     // Post the new user to the backend
     const response = await fetch('http://localhost:8000/api/users/register', {
         method: 'POST',
@@ -26,24 +25,19 @@ const fetchPostNewUser = async (newUser: User) => {
         },
         body: JSON.stringify(newUser)
     });
-
     if (!response.ok) {
         throw new Error(`Error: ${response.status} - ${response.statusText}`);
     }
-
     return response.json();
 };
-
 
 const getLatestUserId = (existingUsers: User[]) => {
     if (existingUsers.length === 0) {
       return 0;
     }
-  
     const latestUser = existingUsers.reduce((prevUser, currentUser) => {
       return currentUser.id > prevUser.id ? currentUser : prevUser;
     });
-  
     return latestUser.id;
 };
 
@@ -51,7 +45,6 @@ function getSignUpFormData(): User {
     const usernameInput = document.getElementById("signUp-username") as HTMLInputElement;
     const passwordInput = document.getElementById("signUp-password") as HTMLInputElement;
     const emailInput = document.getElementById("signUp-email") as HTMLInputElement;
-
     const formData: User = {
         id: 0, // default value
         username: usernameInput.value,
@@ -59,27 +52,57 @@ function getSignUpFormData(): User {
         email: emailInput.value,
         role: 'user',
     };
-
     return formData;
 }
 
-
-
 document.getElementById("signUp-form")?.addEventListener("submit", async (event) => {
-    // Prevent the default form submission behavior
     event.preventDefault();
-
-    // Get the form data using the getSignUpFormData function
     const formData = getSignUpFormData();
-
-    // Call fetchPostNewUser to add the new user to the backend
     try {
         await fetchPostNewUser(formData);
-
-        // Optionally, you can perform any actions after a successful registration here
-        console.log("User registered successfully!");
+        console.log("User registered successfully!"); 
     } catch (error) {
-        // Handle errors if the registration fails
-        console.error("Error registering user:", error);
+        console.error("Error registering user:", error); 
     }
 });
+
+// LOGIN A USER
+
+const fetchLoginUser = async (loginData: LoginUser) => {
+    const response = await fetch('http://localhost:8000/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+    });
+    if (response.ok) {
+        const user = await response.json();
+        return user;
+    } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+};
+
+function getLoginFormData(): LoginUser {
+    const usernameInput = document.getElementById("username") as HTMLInputElement;
+    const passwordInput = document.getElementById("password") as HTMLInputElement;
+    const formData: LoginUser = {
+        username: usernameInput.value,
+        password: passwordInput.value,
+    };
+    return formData;
+}
+
+document.getElementById("login-form")?.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = getLoginFormData();
+    try {
+        const user = await fetchLoginUser(formData);
+        console.log("User logged in successfully!", user);
+    } catch (error) {
+        console.error("Error logging in:", error);
+    }
+});
+
